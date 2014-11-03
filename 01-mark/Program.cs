424d7f55d4414385135e9cfe40782694c7d869db
fakefeik <feikfake1@gmail.com>
2014-11-03 16:50:29 +0500

    Initial commit

---
+++ 01-mark/Program.cs
@@ -0,0 +1,34 @@
+﻿using System.IO;
+
+namespace _01_mark
+{
+    class Program
+    {
+        static string ReadFromFile(string filename)
+        {
+            string data;
+            using (var reader = new StreamReader(filename))
+            {
+                data = reader.ReadToEnd();
+            }
+            return data;
+        }
+
+        static void WriteToFile(string filename, string data)
+        {
+            using (var writer = new StreamWriter(filename))
+            {
+                writer.Write(data);
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            var input = args[0];
+            var output = args[1];
+            var data = ReadFromFile(input);
+            var html = MarkdownProccessor.ToHtml(data);
+            WriteToFile(output, html);
+        }
+    }
+}
