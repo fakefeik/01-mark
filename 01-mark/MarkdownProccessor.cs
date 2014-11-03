0d98bb0927e30602e73362f4e0ef0c2abf6e0bdb
fakefeik <feikfake1@gmail.com>
2014-11-03 18:30:16 +0500

    Added 'red' paragraph test

--- 01-mark/MarkdownProccessor.cs
+++ 01-mark/MarkdownProccessor.cs
@@ -2,15 +2,37 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
 namespace _01_mark
 {
     class MarkdownProccessor
     {
+        private static string AddHeader(string text)
+        {
+            const string header = @"<!DOCTYPE html>
+<html>
+<head>
+<meta http-equiv=""Content-Type"" content=""text/html; charset=utf-8"">
+</head>
+<body>
+{0}
+</body>
+</html>";
+            return string.Format(header, text);
+        }
+
+        public static string AddParagraphs(string text)
+        {
+            return string.Format("<p>{0}</p>", text);
+        }
+
         public static string ToHtml(string text)
         {
-            return text;
+            return AddHeader(text);
+            
         }
+
     }
 }
---
+++ 01-mark/MarkdownProcessorTest.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit;
+using NUnit.Framework;
+
+namespace _01_mark
+{
+    [TestFixture]
+    class MarkdownProcessorTest
+    {
+        const string header = @"<!DOCTYPE html>
+<html>
+<head>
+<meta http-equiv=""Content-Type"" content=""text/html; charset=utf-8"">
+</head>
+<body>
+{0}
+</body>
+</html>";
+
+        [Test]
+        public void Test_empty()
+        {
+            const string empty = @"<!DOCTYPE html>
+<html>
+<head>
+<meta http-equiv=""Content-Type"" content=""text/html; charset=utf-8"">
+</head>
+<body>
+
+</body>
+</html>";
+            Console.Write(MarkdownProccessor.ToHtml(""));
+            Assert.AreEqual(empty, MarkdownProccessor.ToHtml(""));
+        }
+
+        [Test]
+        public void Test_paragraph()
+        {
+            var text = "sometext\n\nNew Parahraph\n\nsomeothertext";
+            var textFormatted = "sometext<p>NewParahraph</p>someothertext";
+            Assert.AreEqual(string.Format(header, textFormatted), MarkdownProccessor.ToHtml(text));
+        }
+    }
+}
