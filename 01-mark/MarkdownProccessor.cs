d4c863a66ed7a7d42aed48f29f559ef6c89df8b5
fakefeik <feikfake1@gmail.com>
2014-11-07 10:22:38 +0500

    added <em> tag tests

--- 01-mark/MarkdownProccessor.cs
+++ 01-mark/MarkdownProccessor.cs
@@ -1,15 +1,22 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.ExceptionServices;
+using System.Runtime.InteropServices;
 using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using System.Text.RegularExpressions;
 using System.Threading.Tasks;
+using NUnit.Framework.Constraints;
 
 namespace _01_mark
 {
     class MarkdownProccessor
     {
+        private static Stack<int> strings = new Stack<int>(); 
+        private static Dictionary<string, string> tags = new Dictionary<string, string> { { "_", "<em>"}, { "__", "<strong>" }, { "`", "<code>" } }; 
+
+
         private static string AddHeader(string text)
         {
             const string header = @"<!DOCTYPE html>
@@ -24,9 +31,16 @@ namespace _01_mark
             return string.Format(header, text);
         }
 
+        //TODO
+        private static string Reformat(string text)
+        {
+
+            return text;
+        }
+
         public static string AddParagraphs(string text)
         {
-            return String.Join("", Regex.Split(text, @"\n\r*\s*\n").Select(x => string.Format("<p>{0}</p>", x)));
+            return String.Join("", Regex.Split(text, @"\n\r*\s*\n").Select(x => string.Format("<p>{0}</p>", x)).Select(Reformat));
         }
 
         public static string ToHtml(string text)
@@ -35,6 +49,5 @@ namespace _01_mark
                 return AddHeader("");
             return AddHeader(AddParagraphs(text));
         }
-
     }
 }
--- 01-mark/MarkdownProcessorTest.cs
+++ 01-mark/MarkdownProcessorTest.cs
@@ -9,9 +9,9 @@ using NUnit.Framework;
 namespace _01_mark
 {
     [TestFixture]
-    class MarkdownProcessorTest
+    internal class MarkdownProcessorTest
     {
-        const string header = @"<!DOCTYPE html>
+        private const string header = @"<!DOCTYPE html>
 <html>
 <head>
 <meta http-equiv=""Content-Type"" content=""text/html; charset=utf-8"">
@@ -47,6 +47,30 @@ namespace _01_mark
         {
             var text = "sometext  \n\r    \n\r\t   NewText  \n   \n other";
             var textFormatted = "<p>sometext  </p><p>\r\t   NewText  </p><p> other</p>";
+            Assert.AreEqual(string.Format(header, textFormatted), MarkdownProccessor.ToHtml(text));
+        }
+
+        [Test]
+        public void Test_simple_em_tags()
+        {
+            var text = "some _statement other_ statement";
+            var textFormatted = "<p>some <em>statement other</em> statement</p>";
+            Assert.AreEqual(string.Format(header, textFormatted), MarkdownProccessor.ToHtml(text));
+        }
+
+        [Test]
+        public void Test_em_tags_inside_words()
+        {
+            var text = "Test_em_tags_inside_words";
+            var textFormatted = "<p>Test_em_tags_inside_words</p>";
+            Assert.AreEqual(string.Format(header, textFormatted), MarkdownProccessor.ToHtml(text));
+        }
+
+        [Test]
+        public void Test_ref()
+        {
+            var text = "ss \\_abc cde\\_ asd";
+            var textFormatted = "<p>ss _abc cde_ asd</p>";
             Assert.AreEqual(string.Format(header, textFormatted), MarkdownProccessor.ToHtml(text));
         }
     }
