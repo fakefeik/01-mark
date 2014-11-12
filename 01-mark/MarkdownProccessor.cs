d1dadeb14107aadececf61e244a5969edd155ae0
fakefeik <feikfake1@gmail.com>
2014-11-13 01:51:44 +0500

    >tfw tests are finally green.

--- 01-mark/MarkdownProccessor.cs
+++ 01-mark/MarkdownProccessor.cs
@@ -14,15 +14,16 @@ namespace _01_mark
 {
     class MarkdownProccessor : IMarkdownProcessor
     {
-        private static Stack<int> strings = new Stack<int>();
-        private static string open = "<{0}>";
-        private static string close = "</{0}>";
-        private static Dictionary<string, string> tags = new Dictionary<string, string> 
+        /*private static Dictionary<string, string> tags = new Dictionary<string, string> 
         { 
             { "_", "em"}, 
             { "__", "strong" }, 
             { "`", "code" } 
-        }; 
+        }; */
+
+        private static string open = "<{0}>";
+        private static string close = "</{0}>";
+        private static Stack<Tuple<string, int>> strings = new Stack<Tuple<string, int>>(); 
         private static Dictionary<string, string> escape = new Dictionary<string, string>
         {
             //TODO: add some other stuff
@@ -45,26 +46,60 @@ namespace _01_mark
             return string.Format(header, text);
         }
 
-        //TODO
+        //TODO: refactor this
         private static string Reformat(string text)
         {
             for (int i = 0; i < text.Length; i++)
             {
-                if (text[i] == '_')
+                if (text[i] == '`')
                 {
-                    if (strings.Count == 0 && (i == 0 || text[i - 1] == ' '))
-                        strings.Push(i);
-                    else if (strings.Count != 0 && (i == text.Length - 1 || text[i + 1] == ' '))
+                    if ((strings.Count == 0 || strings.Peek().Item1 != "`") && (i == 0 || IsNotInWord(text[i - 1])))
+                        strings.Push(new Tuple<string, int>("`", i));
+                    else if (strings.Count != 0 && strings.Peek().Item1 == "`" && (i == text.Length - 1 || IsNotInWord(text[i + 1])))
                     {
-                        var prev = strings.Pop();
-                        text = text.Remove(i, 1);
-                        text = text.Remove(prev, 1);
-                        text = text.Insert(i - 1, "</em>");
-                        text = text.Insert(prev, "<em>");
+                        var prev = strings.Pop().Item2;
+                        text = InsertTags(text, "code", prev, i, 1);
+                    }
+                }
+                if (text[i] == '_' && text[i + 1] == '_')
+                {
+                    if (strings.Count != 0 && strings.Peek().Item1 == "`")
+                        continue;
+                    if ((strings.Count == 0 || strings.Peek().Item1 != "__") && (i == 0 || IsNotInWord(text[i - 1])))
+                        strings.Push(new Tuple<string, int>("__", i));
+                    else if (strings.Count != 0 && strings.Peek().Item1 == "__" && (i == text.Length - 2 || IsNotInWord(text[i + 2])))
+                    {
+                        var prev = strings.Pop().Item2;
+                        text = InsertTags(text, "strong", prev, i, 2);
+                    }
+                }
+                else if (text[i] == '_')
+                {
+                    if (strings.Count != 0 && strings.Peek().Item1 == "`")
+                        continue;
+                    if ((strings.Count == 0 || strings.Peek().Item1 != "_") && (i == 0 || IsNotInWord(text[i - 1])))
+                        strings.Push(new Tuple<string, int>("_", i));
+                    else if (strings.Count != 0 && strings.Peek().Item1 == "_" && (i == text.Length - 1 || IsNotInWord(text[i + 1])))
+                    {
+                        var prev = strings.Pop().Item2;
+                        text = InsertTags(text, "em", prev, i, 1);
                     }
                 }
             }
-            text = Regex.Replace(text, @"\\", "");
+            return text;
+        }
+
+        private static bool IsNotInWord(char c)
+        {
+            return !char.IsLetterOrDigit(c) && c != '\\' && c != '_';
+        }
+
+        private static string InsertTags(string text, string tag, int from, int to, int toDelete)
+        {
+            text = text.Remove(to, toDelete);
+            text = text.Insert(to, string.Format(close, tag));
+            text = text.Remove(from, toDelete);
+            text = text.Insert(from, string.Format(open, tag));
             return text;
         }
 
@@ -82,12 +117,13 @@ namespace _01_mark
             return String.Join("", Regex.Split(text, @"\n\r*\s*\n")
                 .Select(ReplaceHtmlEscapeCharacters)
                 .Select(x => string.Format("<p>{0}</p>", x))
-                .Select(Reformat));
+                .Select(Reformat)
+                .Select(x => Regex.Replace(x , @"\\", "")));
         }
 
         public string ToHtml(string text)
         {
-            strings = new Stack<int>();
+            strings = new Stack<Tuple<string, int>>();
             if (string.IsNullOrEmpty(text))
                 return AddHeader("");
             return AddHeader(AddParagraphs(text));
